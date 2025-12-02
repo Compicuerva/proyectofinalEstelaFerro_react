90c5f90b631e580b1dbf2f676e60f0465044ae75
Compicuerva <estelaferro7@gmail.com>
2025-12-01 21:35:45 -0300

    Add files via upload

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+        <hr />
+        <p>&copy; 2025 - Mi Aplicación React
+            Desarrolado por: Estela Ferro</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Inicio() {
+  return (
+    <>
+    <div style={{textAlign: 'center'}}>
+    <h1>| Te damos la Bienvenida a Moksha Joyas |  </h1>
+    <hr />
+    <main>
+      <p><strong>Moksha Joyas</strong> es una tienda especial atendida con mucha pasión.</p> 
+      <p>Nos dedicamos a la creación y venta de joyas artesanales únicas, hechas a mano con materiales de alta calidad.</p>
+      <p>Nuestra misión es ofrecer piezas que no solo embellecen, sino que también cuentan una historia y reflejan la personalidad de quien las lleva.</p>
+      <p>Creemos en la importancia de la artesanía y el diseño personalizado, por lo que cada joya es cuidadosamente elaborada para satisfacer los gustos y preferencias de nuestros clientes.</p>
+      <p>En Moksha Joyas, nos esforzamos por brindar una experiencia de compra excepcional, combinando la tradición artesanal con las tendencias contemporáneas.</p>
+      <p>Te invitamos a explorar nuestra colección y descubrir la joya perfecta que complementará tu estilo y celebrará tus momentos especiales.</p>
+    </main>
+
+    <video width="400" height="200" controls>
+        <source src="/video.mp4" type="video/mp4" />
+        Tu navegador no soporta el elemento de video.
+      </video>
+
+    </div>
+    </>
+  )
+}
+
+export default Inicio
